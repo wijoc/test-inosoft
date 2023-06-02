4c172e33beaf6e331e8ae283c8b8fe7f6d8ec006
Joe <wicaksonojoshua@gmail.com>
2023-06-02 18:08:21 +0700

    first commit

---
+++ app/Http/Controllers/MotorController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Motor;
+use Illuminate\Http\Request;
+
+class MotorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Motor  $motor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Motor $motor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Motor  $motor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Motor $motor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Motor  $motor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Motor $motor)
+    {
+        //
+    }
+}
